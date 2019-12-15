0b2c80a501cedf03f7d8915f817854473f07603c
Ken Tam <ktam33@gmail.com>
2019-12-15 09:06:31 -0600

    day2 stuff

--- 2/day2part1.scala
+++ 2/day2part1.scala
@@ -3,8 +3,13 @@ import scala.io.Source
 val filename = "input.txt"
 
 val line = Source.fromFile(filename).getLines.toList.apply(0)
+var numbers = line.split(",").map(_.toInt)
+numbers = numbers.updated(1, 12)
+numbers = numbers.updated(2, 2)
+numbers = processInstruction(0, numbers)
+numbers.foreach(x => print(x + " "))
+println("")
 
-val numbers = line.split(",").map(_.toInt)
-println(numbers(0))
-
-println(numbers.getClass)
+def processInstruction(startPosition: Int, x: List[Int]): List[Int] = {
+    List(1,2,3)
+}
