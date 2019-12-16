54e69daa570fbbd280413a3bf836c192a5f0425e
Ken Tam <ktam33@gmail.com>
2019-12-15 23:51:55 -0600

    solves day 2 part 1

--- 2/day2part1.scala
+++ 2/day2part1.scala
@@ -3,13 +3,25 @@ import scala.io.Source
 val filename = "input.txt"
 
 val line = Source.fromFile(filename).getLines.toList.apply(0)
-var numbers = line.split(",").map(_.toInt)
+var numbers = line.split(",").map(_.toInt).toList
 numbers = numbers.updated(1, 12)
 numbers = numbers.updated(2, 2)
 numbers = processInstruction(0, numbers)
 numbers.foreach(x => print(x + " "))
 println("")
 
-def processInstruction(startPosition: Int, x: List[Int]): List[Int] = {
-    List(1,2,3)
+def processInstruction(startPosition: Int, instructions: List[Int]): List[Int] = {
+    val opCode = instructions(startPosition)
+    opCode match {
+        case 99 => instructions
+        case 1 => processInstruction(startPosition + 4, doOp(startPosition, instructions, (x, y) => x + y))
+        case 2 => processInstruction(startPosition + 4, doOp(startPosition, instructions, (x, y) => x * y))
+    }
+}
+
+def doOp(startPosition: Int, instructions: List[Int], op: (Int, Int) => Int): List[Int] = {
+    val num1 = instructions(instructions(startPosition + 1))
+    val num2 = instructions(instructions(startPosition + 2))
+    val replacePosition = instructions(startPosition + 3)
+    instructions.updated(replacePosition, op(num1, num2))
 }
