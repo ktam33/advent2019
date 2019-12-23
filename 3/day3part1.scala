ad05f0f9e6be2f56de53711bce461a9033ddb320
Ken Tam <ken.tam@relativity.com>
2019-12-23 12:21:31 -0600

    gets all points for a wire

--- 3/day3part1.scala
+++ 3/day3part1.scala
@@ -8,15 +8,31 @@ val wire2 = input(1).split(",").toList
 println(wire1)
 println(wire2)
 
-println(getPointsForLine((1,1), "U10"))
+println(getPoints((0,0), wire1))
 
-def getPoints(directions: List[String]): List[Tuple2[Int, Int]] = {
-    val startPoint = (0,0) 
-    List((1,1))
+def getPoints(startPoint: Tuple2[Int, Int], directions: List[String]): Set[Tuple2[Int, Int]] = {
+    var result = Set[Tuple2[Int, Int]]()
+    var lastPoint = startPoint
+    directions.foreach(direction => {
+        var linePoints = getPointsForLine(lastPoint, direction)
+        result = result ++ linePoints
+        lastPoint = linePoints(0)
+    })
+    result
 }
 
 def getPointsForLine(startPoint: Tuple2[Int,Int], instruction: String): List[Tuple2[Int,Int]] = {
     val direction = instruction.take(1)
     val magnitude = instruction.takeRight(instruction.length - 1).toInt
-    List((magnitude, magnitude))
+    var result = List[Tuple2[Int, Int]]()
+    for (i <- 1 to magnitude) {
+        val lastPoint = if (i == 1) startPoint else result(0)
+        direction match {
+            case "U" => result = (lastPoint._1, lastPoint._2 + 1) :: result
+            case "D" => result = (lastPoint._1, lastPoint._2 - 1) :: result
+            case "L" => result = (lastPoint._1 - 1, lastPoint._2) :: result
+            case "R" => result = (lastPoint._1 + 1, lastPoint._2) :: result
+        }
+    }
+    result
 }
