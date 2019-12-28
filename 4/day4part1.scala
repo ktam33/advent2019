e7304a894f157ea14baff7d3deb02126724604ac
Ken Tam <ktam33@gmail.com>
2019-12-28 12:48:10 -0600

    finishes day4 part2

--- 4/day4part1.scala
+++ 4/day4part1.scala
@@ -1,7 +1,7 @@
-//val numbers = 145852 to 616942
-val numbers = 100 to 110
+val numbers = 145852 to 616942
+//val numbers = 100 to 110
 //numbers.foreach(println)
-println(numbers.filter(isAnswer))
+println(numbers.filter(isAnswer).size)
 
 def isAnswer(number: Int) : Boolean = {
     var input = number
@@ -11,13 +11,23 @@ def isAnswer(number: Int) : Boolean = {
         input /= 10
     }
     
-    if (digits.size <> 6) {
-        return false
-    }
+    return isSixDigits(digits) && hasConsecutiveIdentical(digits) && isIncreasingDigits(digits)
+}
 
-    var isValid = false
-    for (x <- 1 to digits.size < 1) {
+def isSixDigits(digits: List[Int]) : Boolean = {
+    digits.size == 6
+}
 
+def hasConsecutiveIdentical(digits: List[Int]) : Boolean = {
+    for (x <- 1 to digits.size - 1) {
+        if (digits(x - 1) == digits(x)) return true
     }
-    return isValid
+    return false
+}
+
+def isIncreasingDigits(digits: List[Int]) : Boolean = {
+    for (x <- 1 to digits.size - 1) {
+        if (digits(x) < digits(x - 1)) return false    
+    }
+    return true
 }
